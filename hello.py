42afe870d6f2179aef356b9fa74fd4675d697710
Parul Sethi <parul1sethi@gmail.com>
2016-02-07 19:05:54 +0530

    Update hello.py
    Average distance frequency with noise filter ,avg_dis vs opt_dis graph

--- hello.py
+++ hello.py
@@ -1,19 +1,25 @@
 import networkx as nx
 import random
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import math
 from mpl_toolkits.mplot3d import Axes3D
+import scipy.signal
 
 random.seed(1)
 
 #: [s, d]
 configs = [
-	[(0, 0), (0,2)],
-	[(0, 0), (3,1)],
-	[(0, 0), (2,4)],
-	[(0,0), (1,3)],
-	[(0,0), (3,5)],
+	[(0, 0), (0,1)], 
+	[(0, 0), (0,2)], 
+	[(0,0), (1,2)], 
+	[(0, 0), (3,1)], 
+	[(0, 0), (3,2)], 
+	[(0, 0), (2,4)], 
+	[(0, 0), (3,4)], 
+	[(0,0), (3,5)],  
+	[(0,0), (4,5)], 
+	[(0,0), (5,5)]
 ]
 
 L=6
@@ -43,10 +49,7 @@ def route(config):
 		current_node = nxt
 		count+=1
 	path.append(d)
-	print(path)
-	print(len(path)-1)
 	return path, len(path)-1
-
 
 
 def route_length_iterator(config,no_of_iteration):
@@ -55,59 +58,61 @@ def route_length_iterator(config,no_of_iteration):
 		yield pl
 
 
-def optimal_vs_average_distance(config,no_of_iteration):
-	x2 = config[1][0]
-	x1 = config[0][0]
-	y2 = config[1][1]
-	y1 = config[0][1]
-	optimal_distance = 2*((x2-x1)+(y2-y1))
-	seq = [ x for x in route_length_iterator(config,no_of_iteration)]
-	average_distance = sum(seq)/len(seq)
-	print(seq)
-	print(average_distance)
-	return optimal_distance,average_distance
-
-
-def plot_optimal_vs_average_distance(no_of_iteration):
-
-	to_plot = [optimal_vs_average_distance(i,no_of_iteration) for i in configs]
-	optimal_distance = [x[0] for x in to_plot]
-	average_distance = [x[1] for x in to_plot]
-	plt.plot(optimal_distance,average_distance)
-	plt.show()
-
-def optimal_vs_deviation(config,no_of_iteration):
+def optimal_distance(config):
 
 	x2 = config[1][0]
 	x1 = config[0][0]
 	y2 = config[1][1]
 	y1 = config[0][1]
 	optimal_distance = 2*((x2-x1)+(y2-y1))
-	dev = [ y-optimal_distance for y in route_length_iterator(config,no_of_iteration)]
-	deviation = sum(dev)/len(dev)
-	return optimal_distance,deviation
+	return optimal_distance
 
+def average_distance(config,no_of_iteration):
 
-def plot_optimal_vs_deviation(no_of_iteration):
-
-	to_plot = [optimal_vs_deviation(i,no_of_iteration) for i in configs]
-	optimal_distance = [x[0] for x in to_plot]
-	deviation = [x[1] for x in to_plot]
-	plt.plot(optimal_distance,deviation)
-	plt.show()
+	avg = [ y for y in route_length_iterator(config,no_of_iteration)]
+	avg_dis = sum(avg)/len(avg)
+	return avg_dis
 
 
 def path_length_distribution(dis_interval,config,no_of_iteration):
 
 	q = [math.floor(_/dis_interval)*dis_interval for _ in route_length_iterator(config,no_of_iteration)]
-	print(q)
 	b = [q.count(_) for _ in range(0,max(q)+dis_interval,dis_interval)]
 	print(b)
 	plt.plot(b)
 	plt.show()
 
 
+def average_distance_freq(config,max_limit_iteration):
+	freq_avg = []
+	for i in range(1,max_limit_iteration):
+		avg_dis = average_distance(config,i)
+		freq_avg.append(avg_dis)
+
+	freq_avg = scipy.signal.savgol_filter(freq_avg, 51,0)
+	plt.plot(freq_avg)
+
+	
+def loop_average_distance_freq(max_limit_iteration):
+
+	for config in configs:
+		average_distance_freq(config,max_limit_iteration)
+	plt.show()
+
+
+def FreqAvg_vs_FreqOpt(no_of_iteration):
+
+	x = []
+	y = []
+	for config in configs:
+		x.append(optimal_distance(config))
+		y.append(average_distance(config,no_of_iteration))
+
+	y = scipy.signal.savgol_filter(y,3,0)
+	plt.plot(x,y)
+	plt.show()
+
 
 if __name__ == "__main__":
-	no_of_iteration = 100
-	path_length_distribution(5,configs[0],no_of_iteration)
+	print("	keep calm and wait! ")
+	FreqAvg_vs_FreqOpt(900)
